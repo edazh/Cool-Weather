2620bad52574147fd73ddfc0c4849417c7ec01df
edazh <edazh@qq.com>
2017-06-08 13:27:25 +0800

    新增切换城市和手动更新天气功能

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -5,9 +5,13 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -41,6 +45,9 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView sportText;
     private TextView qltyText;
     private ImageView imgBingPic;
+    public SwipeRefreshLayout swipeRefreshLayout;
+    private Button btnNavHome;
+    public DrawerLayout drawerLayout;
 
     @Override
 
@@ -53,13 +60,15 @@ public class WeatherActivity extends AppCompatActivity {
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = preferences.getString("weather", null);
+        final String weatherId;
         if (weatherString != null) {
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            weatherId = weather.basic.cityId;
             showWeatherInfo(weather);
         } else {
             //无缓存 去服务器查询天气
-            String weatherId = getIntent().getStringExtra("weather_id");
+            weatherId = getIntent().getStringExtra("weather_id");
             //weatherLayout.setVisibility(View.INVISIBLE);//隐藏scrollview
             resquestWeather(weatherId);
         }
@@ -69,6 +78,15 @@ public class WeatherActivity extends AppCompatActivity {
         } else {
             Glide.with(this).load(bingPic).into(imgBingPic);
         }
+
+        //下拉刷新
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                resquestWeather(weatherId);
+            }
+        });
+
     }
 
 
@@ -104,7 +122,7 @@ public class WeatherActivity extends AppCompatActivity {
      *
      * @param weatherId 城市id
      */
-    private void resquestWeather(String weatherId) {
+    public void resquestWeather(String weatherId) {
         String weatherUrl = "https://free-api.heweather.com/v5/weather?city=" + weatherId + "&key=9b2aea2ada0c4f269fc4a5ea597bd1cb";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
@@ -113,7 +131,9 @@ public class WeatherActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         showToast("获取天气信息失败");
+                        swipeRefreshLayout.setRefreshing(false);
                     }
+
                 });
             }
 
@@ -132,6 +152,7 @@ public class WeatherActivity extends AppCompatActivity {
                         } else {
                             showToast("获取天气信息失败");
                         }
+                        swipeRefreshLayout.setRefreshing(false);
                     }
                 });
             }
@@ -204,6 +225,16 @@ public class WeatherActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        btnNavHome = (Button) findViewById(R.id.title_btn_nav_home);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        btnNavHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
 
